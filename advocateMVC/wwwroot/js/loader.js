e59402e139c59dbaed85b6e028b0d16f68e791d8
BIT\\a.rudich <rudichalex@yandex.ru>
2018-12-27 16:52:02 +0300

    add parallax and little bit another css code

--- advocateMVC/wwwroot/js/loader.js
+++ advocateMVC/wwwroot/js/loader.js
@@ -1,3 +1,7 @@
 ﻿window.onload = function () {
     document.getElementById('loader').style.display = 'none';
-}
+};
+
+//window.onload = setTimeout(function () {
+//    document.getElementById('fading').style.display = 'none';
+//}, 2000);
