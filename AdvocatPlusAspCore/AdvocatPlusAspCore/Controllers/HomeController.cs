22d634a7b73711bab5083f12a5d8d4043c933bbd
BIT\\a.rudich <rudichalex@yandex.ru>
2019-03-20 11:23:33 +0300

    rename About page to Payment page

--- AdvocatPlusAspCore/AdvocatPlusAspCore/Controllers/HomeController.cs
+++ AdvocatPlusAspCore/AdvocatPlusAspCore/Controllers/HomeController.cs
@@ -19,15 +19,15 @@ namespace AdvocatPlusAspCore.Controllers
             EmailService = _emailService;
         }
         [HttpGet]
-        //[Route("")] //need for highlighting <li> index by css
-        //[Route("Home")]
-        //[Route("Home/Index")]
+        [Route("")] //need for highlighting <li> index by css
+        [Route("Home")]
+        [Route("Home/Index")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Payment()
         {
             return View();
         }
